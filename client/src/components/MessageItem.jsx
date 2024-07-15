543a88fe9eedcee7a3a0f3b9b33ac8728d275869
duckies <voducminh39@gmail.com>
2024-07-15 18:01:48 +0700

    load message

---
+++ client/src/components/MessageItem.jsx
@@ -0,0 +1,28 @@
+const MessageItem = ({ value, username }) => {
+  let name = localStorage.getItem("username");
+  return (
+    // <>
+    //   <h1>{value}</h1>
+    // </>
+    <>
+      <div className="p-2 flex-grow-1 d-flex flex-column">
+        <div
+          style={{
+            color: "white",
+            alignSelf: name == username ? "flex-end" : "flex-start",
+            backgroundColor: name == username ? "#0d1d46" : "#6A6E7D",
+            borderRadius: "10px",
+            padding: "10px",
+            margin: "5px",
+            maxWidth: "60%",
+          }}
+          className="message"
+        >
+          {value}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MessageItem;
