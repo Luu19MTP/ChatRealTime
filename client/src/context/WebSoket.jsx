28ebd539c1b4391702ba4c1bc250c9f7bdc38615
duckies <voducminh39@gmail.com>
2024-07-16 12:38:27 +0700

    fix reload

--- client/src/context/WebSoket.jsx
+++ client/src/context/WebSoket.jsx
@@ -1,128 +1,3 @@
-// import {
-//   useContext,
-//   useState,
-//   useEffect,
-//   createContext,
-//   useRef,
-//   useCallback,
-// } from "react";
-// const WebSocketContext = createContext();
-// const WebSocketProvider = ({ children }) => {
-//   const [connection, setConnection] = useState(false);
-//   const wsRef = useRef(null);
-//   const [response, setResponse] = useState(null);
-//   const [name, setName] = useState(null);
-//   const [users, setUsers] = useState(null);
-//   // console.log("name", name);
-
-//   useEffect(() => {
-//     const ws = new WebSocket("ws://140.238.54.136:8080/chat/chat");
-//     wsRef.current = ws;
-//     setConnection(true);
-//     ws.addEventListener("open", () => {
-//       console.log("WebSocket connection opened");
-//     });
-//     // dữ liệu trả về là json
-//     ws.addEventListener("message", (event) => {
-//       const res = JSON.parse(event.data);
-//       setResponse(res);
-//       console.log("websocket said:", res);
-//     });
-
-//     ws.addEventListener("close", () => {
-//       console.log("Connection is close");
-//       setConnection(false);
-//     });
-//     return () => {
-//       ws.close();
-//     };
-//   }, []);
-
-//   const SendMessage = (msg) => {
-//     if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
-//       wsRef.current.send(JSON.stringify(msg));
-//     }
-//   };
-
-//   const RegisterContext = (username, password) => {
-//     const register_msg = {
-//       action: "onchat",
-//       data: {
-//         event: "REGISTER",
-//         data: {
-//           user: username,
-//           pass: password,
-//         },
-//       },
-//     };
-//     SendMessage(register_msg);
-//   };
-
-//   const LoginContext = (usename, password) => {
-//     const login_msg = {
-//       action: "onchat",
-//       data: {
-//         event: "LOGIN",
-//         data: {
-//           user: usename,
-//           pass: password,
-//         },
-//       },
-//     };
-//     SendMessage(login_msg);
-//   };
-
-//   const LogoutContext = () => {
-//     const logout_msg = {
-//       action: "onchat",
-//       data: {
-//         event: "LOGOUT",
-//       },
-//     };
-//     SendMessage(logout_msg);
-//   };
-
-//   const GetUserList = useCallback(() => {
-//     return new Promise((resolve) => {
-//       const getuser_msg = {
-//         action: "onchat",
-//         data: {
-//           event: "GET_USER_LIST",
-//         },
-//       };
-//       SendMessage(getuser_msg);
-//       if (connection) {
-//         wsRef.current.addEventListener("message", (event) => {
-//           const res = JSON.parse(event.data);
-//           resolve(res.data);
-//           wsRef.current.removeEventListener("message", this);
-//         });
-//       }
-//       console.log("chay 1 lan ben websocket");
-//     });
-//   }, [connection]);
-
-  
-
-//   const value = {
-//     connection,
-//     response,
-//     name,
-//     users,
-//     GetUserList,
-//     setResponse,
-//     setName,
-//     RegisterContext,
-//     LoginContext,
-//     LogoutContext,
-//   };
-//   return (
-//     <WebSocketContext.Provider value={value}>
-//       {children}
-//     </WebSocketContext.Provider>
-//   );
-// };
-// export { WebSocketContext, WebSocketProvider };
 import {
   useContext,
   useState,
@@ -140,13 +15,14 @@ const WebSocketProvider = ({ children }) => {
   const [response, setResponse] = useState(null);
   const [name, setName] = useState(null);
   const [users, setUsers] = useState(null);
-
+  const [flag, setFlag] = useState(false);
   useEffect(() => {
     const ws = new WebSocket("ws://140.238.54.136:8080/chat/chat");
     wsRef.current = ws;
     setConnection(true);
     ws.addEventListener("open", () => {
       console.log("WebSocket connection opened");
+     
     });
 
     ws.addEventListener("message", (event) => {
@@ -209,6 +85,39 @@ const WebSocketProvider = ({ children }) => {
     SendMessage(logout_msg);
   };
 
+  const Relogin = useCallback(
+    (re_login_code) => {
+      return new Promise((resolve) => {
+        const relogin_msg = {
+          action: "onchat",
+          data: {
+            event: "RE_LOGIN",
+            data: {
+              RE_LOGIN_CODE: re_login_code,
+            },
+          },
+        };
+        SendMessage(relogin_msg);
+  
+        const messageListener = (event) => {
+          const res = JSON.parse(event.data);
+          if (res.status === "success" && res.event === "RE_LOGIN") {
+            localStorage.setItem("login_code", res.data.RE_LOGIN_CODE);
+            setFlag(true);
+            resolve(res.data); // Resolve the promise with data from server
+            wsRef.current.removeEventListener("message", messageListener); // Remove the listener after resolving
+          }
+        };
+  
+        if (connection) {
+          wsRef.current.addEventListener("message", messageListener);
+        }
+      });
+    },
+    [connection, flag]
+  );
+  
+
   const GetUserList = useCallback(() => {
     return new Promise((resolve) => {
       const getuser_msg = {
@@ -226,7 +135,7 @@ const WebSocketProvider = ({ children }) => {
         });
       }
     });
-  }, [connection]);
+  }, [connection, flag]);
 
   const CreateRoom = (roomName) => {
     const create_room_msg = {
@@ -285,6 +194,8 @@ const WebSocketProvider = ({ children }) => {
     response,
     name,
     users,
+    flag,
+    Relogin,
     GetUserList,
     setResponse,
     setName,
--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,93 +1,3 @@
-
-// import { useContext, useEffect, useState } from "react";
-// import ChatList from "../../components/ChatList";
-// import "./Chat.css";
-// import { WebSocketContext } from "../../context/WebSoket";
-// import ChatContent from "../../components/ChatContent";
-
-// const Chat = () => {
-//   const { connection, GetUserList } = useContext(WebSocketContext);
-//   const [users, setUsers] = useState(null);
-//   const [x, setX] = useState(0);
-//   const [username, setUsername] = useState(null);
-//   function handleClickToSaveName(username) {
-//     setUsername(username);
-//   }
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const result = await GetUserList();
-//         setUsers(result);
-//         console.log("result ben client la:", result); // Sử dụng result mới nhất ở đây
-//       } catch (error) {
-//         console.error("Lỗi khi lấy danh sách người dùng:", error);
-//       }
-//     };
-
-//     if (connection) {
-//       fetchData();
-//     }
-
-//     console.log("chay 1 lan ben chat client");
-//   }, [connection, GetUserList, x]);
-
-//   console.log(username);
-
-//   return (
-//     <>
-//       <button
-//         onClick={() => {
-//           setX(x + 1);
-//         }}
-//       >
-//         {x}
-//       </button>
-//       <div
-//         id="wrapper"
-//         className="container-fluid d-flex rounded-2 border p-1 gap-1"
-//       >
-//         <div className="left col-3 p-2 border-end">
-//           <input
-//             className="form-control me-2"
-//             type="search"
-//             placeholder="Search"
-//             aria-label="Search"
-//           />
-//           <ChatList users={users} saveUsername={handleClickToSaveName} />
-//         </div>
-//         <div
-//           className="border-end right col-7 d-flex flex-column"
-//           style={{ height: 500 }}
-//         >
-//           <div className="info__chat border-bottom">
-//             <div className="avatar">
-//               <i className="fa-solid fa-circle-user fs-2 p-0"></i>
-//             </div>
-//             <div className="flex-grow-1">
-//               <div className="name">{username}</div>
-//               <div className="type">
-//                 <i className="fa-solid fa-people-group"></i>
-//               </div>
-//             </div>
-//             <div className="detail">
-//               <i className="fa-solid fa-circle-info"></i>
-//             </div>
-//           </div>
-
-//           <ChatContent username={username} />
-//         </div>
-//         <div className="col-2">
-//           <div className="member border-bottom d-flex">MEMBER</div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Chat;
-
-
 import { useContext, useEffect, useState } from "react";
 import ChatList from "../../components/ChatList";
 import "./Chat.css";
@@ -95,7 +5,8 @@ import { WebSocketContext } from "../../context/WebSoket";
 import ChatContent from "../../components/ChatContent";
 
 const Chat = () => {
-  const { connection, GetUserList, CreateRoom, JoinRoom } = useContext(WebSocketContext);
+  const { connection, GetUserList, CreateRoom, JoinRoom, flag, Relogin } =
+    useContext(WebSocketContext);
   const [users, setUsers] = useState(null);
   const [x, setX] = useState(0);
   const [username, setUsername] = useState(null);
@@ -103,16 +14,20 @@ const Chat = () => {
   const [roomName, setRoomName] = useState("");
   const [rooms, setRooms] = useState([]);
   const [error, setError] = useState("");
-
+  const [re, setRe] = useState(null);
   function handleClickToSaveName(username) {
     setUsername(username);
   }
 
   useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("login_code"));
+
     const fetchData = async () => {
       try {
         const result = await GetUserList();
+        const re = await Relogin(token);
         setUsers(result);
+        setRe(re);
       } catch (error) {
         console.error("Lỗi khi lấy danh sách người dùng:", error);
       }
@@ -121,11 +36,11 @@ const Chat = () => {
     if (connection) {
       fetchData();
     }
-  }, [connection, GetUserList, x]);
+  }, [connection, x, re]);
 
   const handleCreateRoom = () => {
     if (roomName) {
-      const userNames = users ? users.map(user => user.username) : [];
+      const userNames = users ? users.map((user) => user.username) : [];
       if (rooms.includes(roomName) || userNames.includes(roomName)) {
         setError("Tên phòng đã tồn tại");
       } else {
